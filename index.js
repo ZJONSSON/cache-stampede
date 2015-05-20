9e5fc504afeef27cb972d0d4ed01182ec33e925f
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-05-19 21:46:21 -0400

    Simplify adapters

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 var Stampede = require('./stampede'),
     util = require('util');
 
-function MongoStampede(collection,options) {
-  if (!(this instanceof MongoStampede))
-    return new MongoStampede(collection,options);
-  this.adapter = require('./adapters/mongo')(collection);
-  Stampede.call(this,options);
-}
-util.inherits(MongoStampede,Stampede);
-
-function RedisStampede(collection,options) {
-  if (!(this instanceof RedisStampede))
-    return new RedisStampede(collection,options);
-  this.adapter = require('./adapters/redis')(collection);
-  Stampede.call(this,options);
-}
-util.inherits(RedisStampede,Stampede);
-
-function FileStampede(directory,options) {
-  if (!(this instanceof FileStampede))
-    return new FileStampede(directory,options);
-  this.adapter = require('./adapters/file')(directory);
-  Stampede.call(this,options);
-}
-util.inherits(FileStampede,Stampede);
-
-
-
 module.exports = Stampede;
-module.exports.mongo = MongoStampede;
-module.exports.redis = RedisStampede;
-module.exports.file = FileStampede;
+
+['file','mongo','redis'].forEach(function(key) {
+  var Adapter = function(collection,options) {
+    if (!(this instanceof Adapter))
+      return new Adapter(collection,options);
+    this.adapter = require('./adapters/'+key)(collection,options);
+    Stampede.call(this,options);
+  };
+  util.inherits(Adapter,Stampede);
+  module.exports[key] = Adapter;
+});
