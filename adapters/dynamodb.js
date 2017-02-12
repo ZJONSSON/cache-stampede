b049f8c19175b88b38fb75cf3562485d26a8e3ad
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-02-12 12:16:02 -0500

    Save as object - avoid reserved words by `cs_` prefix

--- adapters/dynamodb.js
+++ adapters/dynamodb.js
@@ -13,18 +13,39 @@ module.exports = function(client,prefix) {
         }
       };
       return client.getAsync(query)
-        .then(d => d.Item && JSON.parse(d.Item.ddata) || undefined);
+        .then(d => {
+          d = d.Item;
+          if (d) return {
+            _id: d.key,
+            data: d.cs_data,
+            __caching__: d.cs_caching,
+            info: d.cs_info,
+            updated : new Date(d.cs_updated),
+            encrypted: d.cs_encrypted,
+            error: d.cs_error,
+            expiryTime: d.cs_expiryTime
+          };
+        });
     },
 
     insert : function(key,d) {
+      d = {
+        id: key,
+        cs_data: d.data,
+        cs_caching: d.__caching__,
+        cs_updated: d.updated.valueOf(),
+        cs_info: d.info,
+        cs_encrypted: d.encrypted || false,
+        cs_error: d.error || false,
+        cs_expiryTime: d.expiryTime
+      };
+
       const query = {
         TableName: prefix,
-        Item: {
-          "id": key,
-          "ddata": JSON.stringify(d)
-        },
+        Item: d,
         ConditionExpression: "attribute_not_exists(id)"
       };
+      
       return client.putAsync(query)
         .catch(err => {
           if (err && err.code === 'ConditionalCheckFailedException')
@@ -40,9 +61,14 @@ module.exports = function(client,prefix) {
         Key: {
           "id": key,
         },
-        UpdateExpression: "set ddata = :d",
+        UpdateExpression: "set cs_data = :cs_data, cs_caching = :cs_caching, cs_encrypted = :cs_encrypted, cs_error = :cs_error, cs_updated =:cs_updated, cs_expiryTime = :cs_expiryTime",
         ExpressionAttributeValues: {
-          ":d": JSON.stringify(d)
+          ":cs_data": d.data,
+          ":cs_caching": d.__caching__,
+          ":cs_encrypted": d.encrypted ||false,
+          ":cs_error": d.error || false,
+          ":cs_updated": (d.updated || new Date()).valueOf(),
+          ":cs_expiryTime": (d.expiryTime && d.expiryTime.valueOf()  || 200000000000000)
         }
       };
       return client.updateAsync(query);
